d86c1093b058a96d193164b572799d3ce9b96b5b
Ben Tran <dataethos@users.noreply.github.com>
2015-02-01 18:31:27 -0800

    Added function implementation for computing the cost function for linear regression with multiple variables.

--- programming-exercises/linear-regression-with-multiple-variables/computeCostMulti.m
+++ programming-exercises/linear-regression-with-multiple-variables/computeCostMulti.m
@@ -13,9 +13,10 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+predictions = X * theta;
+errorTerm = predictions - y;
 
-
-
+J = (1 / (2 * m)) * (errorTerm)' * errorTerm;
 
 % =========================================================================
 
