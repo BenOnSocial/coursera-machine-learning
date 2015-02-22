17e40a2514fbeac4ea9ba089d6e81e7bbac1509d
Ben Tran <dataethos@users.noreply.github.com>
2015-02-21 21:57:47 -0800

    Implemented function to visualize input data.

--- programming-exercises/logistic-regression/plotData.m
+++ programming-exercises/logistic-regression/plotData.m
@@ -12,17 +12,13 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+positive = find(y == 1);
+negative = find(y == 0);
 
-
-
-
-
-
-
+plot(X(positive, 1), X(positive, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 4);
+plot(X(negative, 1), X(negative, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 4);
 
 % =========================================================================
-
-
 
 hold off;
 
