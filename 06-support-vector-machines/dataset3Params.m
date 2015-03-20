eb32af5604eb8ec61a9620f0e7f9e492a4cd7fb9
Ben Tran <dataethos@users.noreply.github.com>
2015-03-20 03:52:08 -0700

    Implemented optimal C and sigma value selection, from a fixed set of values, for dataset 3.

--- 06-support-vector-machines/dataset3Params.m
+++ 06-support-vector-machines/dataset3Params.m
@@ -23,11 +23,32 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% Create a list of values to choose from for C and sigma
+cAndSigmaValues = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+predictionError = [];
 
+% Try all permutations of C and sigma values from the list above
+for cIndex = 1:size(cAndSigmaValues)
+  C = cAndSigmaValues(cIndex);
 
+  for sigmaIndex = 1:size(cAndSigmaValues)
+    sigma = cAndSigmaValues(sigmaIndex);
+    
+    % Train a model using our test dataset
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+    % Compute predictions for the given cross validation set
+    predictions = svmPredict(model, Xval);
 
+    % Compute and collect prediction errors with their associated C and sigma values
+    predictionError = [predictionError; [mean(double(predictions ~= yval)), C, sigma]];
+  endfor
+endfor
 
+% Determine the C and sigma values that resulted in the lowest prediction error
+[minError, minErrorIndex] = min(predictionError(:, 1));
+C = predictionError(minErrorIndex, 2);
+sigma = predictionError(minErrorIndex, 3);
 
 % =========================================================================
 
